77ef38eb05b472db0b68186f1091270bc81ae3c5
Alexander Kirk <alexander@kirk.at>
2013-01-30 17:17:17 +0100

    a little more documentation + whitespace changes

--- thinkery/client.py
+++ thinkery/client.py
@@ -1,19 +1,23 @@
 import requests
-from urllib import quote, urlencode
-from urlparse import parse_qs
-try:
-    import simplejson as json
-except ImportError:
-    import json
+from urllib import quote
 
 class Client(object):
-    site = 'https://api.thinkery.me/v1/'
+	site = 'https://api.thinkery.me/v1/'
 
-    def __init__(self, client_id, client_secret, access_token):
-    	self.r = requests.session(params={'access_token': access_token})
+	def __init__(self, client_id, client_secret, access_token):
+		"""
+		Returns a new Client which can then be used to access the API
+		"""
+		self.r = requests.session(params={'access_token': access_token})
 
-    def get(self, call):
-    	return self.r.get("%s%s" % (self.site, call))
+	def get(self, call):
+		"""
+		Issue a GET request against the thinkery API
+		"""
+		return self.r.get("%s%s" % (self.site, call))
 
-    def post(self, call, params):
-    	return self.r.post("%s%s" % (self.site, quote(call)), params)
+	def post(self, call, params):
+		"""
+		Issue a POST request against the thinkery API
+		"""
+		return self.r.post("%s%s" % (self.site, quote(call)), params)
